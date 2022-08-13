7866debab2ca2df38ae6bbd384fd124d2287aa3b
Eliana Maricel Dominguez <102763298+Lyrsk@users.noreply.github.com>
2022-08-13 18:42:00 -0300

    Ejercicio 9 de Python y 7 de SQL

---
+++ Valoracion_Final/Ejercicio9.py
@@ -0,0 +1,16 @@
+''' Consigna: 
+9) Crear una tupla en Python con el nombre de “Historial5” la cual contenga los siguientes valores:
+    8520, 9510, 7530, 3570, 1590
+Que representa montos de atención en pesos por diferentes servicios/consultas de la mascota “Toto”.
+Crear una función para determinar el valor máximo de atención gastada en “Toto”. '''
+
+
+def suma_gastos(gastos):
+    resultado = sum(gastos)
+    print(f'La suma de todos los gastos de la mascota son: ${resultado} pesos.')
+
+Historial5 = (8520, 9510, 7530, 3570, 1590)
+
+suma_gastos(Historial5)
+
+
