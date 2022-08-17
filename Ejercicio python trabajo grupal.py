9f26b40b6556a34fa3b2e68496b4da8ffde2f37d
AlexisToranzo <toranzoalexis11@gmail.com>
2022-08-17 18:55:37 -0300

    Realize ejercicio de python

---
+++ Ejercicio python trabajo grupal.py
@@ -0,0 +1,7 @@
+# Imprimir toda la información de dueño menos DNI y apellido
+Dueno2 = [23546987,"Maria","Perez",4789689,"Pueyrredon  811"]
+
+for x in Dueno2:
+	if x != 23546987 and x != "Perez":
+		print(x)
+
